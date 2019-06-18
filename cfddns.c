60a447433374ec950e6ecbd8773bb44abe07090d
Chinory <chinory@cup.moe>
2019-06-18 13:55:47 +0800

    finish

--- cfddns.c
+++ cfddns.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <curl/curl.h>
 
-#define URL_BASE "https://api.cloudflare.com/client/v4"
-#define HEAD_EMAIL "X-Auth-Email: "
-#define HEAD_APIKEY "X-Auth-Key: "
-
 #define STR_MAX 254 // max=254
 #define BUF_MAX 1022 // max=65534
-#define NAME_MAX 255
-#define LINE_MAX 2048
+#define NAME_MAX 255  // usually 255 in linux
+#define BUFF_SIZE 2016 // in x86_64 if set to 2016, the context struct will be just 4K size
 
-// #define LOG_SECRETS
+// #define LOG_SECRETS // show apikey, zone_id, record_id in stdout
 
 static inline bool is_value(char c) {
     return c && c != '\n' && c != ' ' && c != '\t' && c != '#';
@@ -43,16 +38,12 @@ static inline char *pass_line(char *i) {
     return i;
 }
 
+#define URL_BASE "https://api.cloudflare.com/client/v4"
+#define HEAD_EMAIL "X-Auth-Email: "
+#define HEAD_APIKEY "X-Auth-Key: "
+
 #define UNUSED __attribute__((unused))
 #define STRLEN(STR) (sizeof(STR) - 1)
-
-static inline void fputss(const char *start, const char *end, FILE *file) {
-    fwrite(start, sizeof(char), end - start, file);
-}
-
-// static inline void fputcn(char c, size_t n, FILE *file) {
-//     for (; n; --n) fputc(c, file);
-// }
 
 char BASENAME[NAME_MAX + 1];
 
@@ -77,11 +68,7 @@ static inline bool string_equals_slice(const string *str, const char *s, size_t 
     return str->len == n && !memcmp(str->data, s, n);
 }
 
-static inline bool string_equals_range(const string *str, const char *s, const char *e) {
-    return string_equals_slice(str, s, e - s);
-}
-
-static inline bool string_equals(const string *str, const string *_str) {
+static inline bool string_equals_string(const string *str, const string *_str) {
     return string_equals_slice(str, _str->data, _str->len);
 }
 
@@ -208,6 +195,10 @@ typedef struct variable {
 } variable;
 
 struct cfddns_context {
+    FILE *fin;
+    FILE *fout;
+    FILE *flog;
+    variable *vars;
     string user_email_header;
     string user_apikey_header;
     string zone_id;
@@ -216,11 +207,7 @@ struct cfddns_context {
     string record_type;
     string record_name;
     string record_content;
-    variable *vars;
-    char line[LINE_MAX];
-    FILE *fin;
-    FILE *fout;
-    FILE *flog;
+    char buff[BUFF_SIZE];
 } cfddns;
 
 static void
@@ -268,8 +255,7 @@ cfddns_get_zone_id(string *zone_id) {
     buffer_concat_string(&url, &cfddns.zone_name);
     buffer_c_str(&url);
 
-    struct curl_slist *headers;
-    headers = cfddns_make_headers();
+    struct curl_slist *headers = cfddns_make_headers();
 
     buffer response;
     buffer_clear(&response);
@@ -313,8 +299,7 @@ cfddns_get_record_id(string *record_id, string *record_content) {
     buffer_concat_string(&url, &cfddns.zone_name);
     buffer_c_str(&url);
 
-    struct curl_slist *headers;
-    headers = cfddns_make_headers();
+    struct curl_slist *headers = cfddns_make_headers();
 
     buffer response;
     buffer_clear(&response);
@@ -420,8 +405,8 @@ cfddns_proc_line_var(char *s, char *e) {
     }
     var = malloc(sizeof(variable));
     if (var == NULL) {
-        fputss(s, e, cfddns.fout);
-        fputss(s, e, cfddns.flog);
+        fwrite(s, sizeof(char), n, cfddns.fout);
+        fwrite(s, sizeof(char), n, cfddns.flog);
         return cfddns_line_done(e, " #need_memory");
     }
     string_copy_slice(&var->key, s, n - 1);
@@ -577,7 +562,7 @@ cfddns_proc_line_record(char *s, char *e) {
     if (!success) {
         if (!cfddns_get_record_id(&cfddns.record_id, &cfddns.record_content)) {
             log = " #get_record_id_failed";
-        } else if (string_equals(&var->value, &cfddns.record_content)) {
+        } else if (string_equals_string(&var->value, &cfddns.record_content)) {
             log = " #already_up_to_date";
             success = true;
         } else if (cfddns_update_record(&var->value)) {
@@ -608,9 +593,9 @@ cfddns_proc_line_record(char *s, char *e) {
 
 static void
 cfddns_proc() {
-    while (fgets(cfddns.line, LINE_MAX, cfddns.fin)) {
+    while (fgets(cfddns.buff, BUFF_SIZE, cfddns.fin)) {
         char *s, *e;
-        s = cfddns_line_next(cfddns.line);
+        s = cfddns_line_next(cfddns.buff);
         e = pass_value(s);
         if (s == e) {
             cfddns_line_done(e, "");
@@ -636,6 +621,12 @@ cfddns_proc() {
     }
 }
 
+void usage(FILE *file) {
+    fputs("Usage: ", file);
+    fputs(BASENAME, file);
+    fputs(" [config_file]\nSee <https://github.com/chinory/cloudflare-ddns-client>\n", file);
+}
+
 int main(int argc, char *argv[]) {
     for (char *i = argv[0], *s = i;;) {
         if (*i) {
@@ -645,51 +636,43 @@ int main(int argc, char *argv[]) {
             break;
         }
     }
-
+    // printf("%lu\n", sizeof(struct cfddns_context));
+    // return 0;
     if (argc < 2) {
-        fputs("Usage: ", stderr);
-        fputs(BASENAME, stderr);
-        fputs(" [config_file]\n", stderr);
+        usage(stderr);
         return 1;
+    } else if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
+        usage(stdout);
+        return 0;
     }
-
     cfddns.fin = fopen(argv[1], "r");
     if (!cfddns.fin) {
         perror(BASENAME);
         return 1;
     }
-
     cfddns.fout = tmpfile();
     if (!cfddns.fout) {
         perror(BASENAME);
         fclose(cfddns.fin);
         return 1;
     }
-
     cfddns.flog = stdout;
-
     cfddns_init();
     cfddns_proc();
     cfddns_cleanup();
-
     fclose(cfddns.fin);
     cfddns.fin = cfddns.fout;
-
-    fclose(cfddns.fout);
-    cfddns.fout = fopen(argv[1], "wb");
-
+    cfddns.fout = fopen(argv[1], "w");
     if (!cfddns.fout) {
         perror(BASENAME);
         fclose(cfddns.fin);
         return 1;
     }
-
     rewind(cfddns.fin);
-    for (int ch; (ch = fgetc(cfddns.fin) != EOF);) {
-        fputc(ch, cfddns.fout);
+    for (size_t n; (n = fread(cfddns.buff, sizeof(char), BUFF_SIZE, cfddns.fin));) {
+        fwrite(cfddns.buff, sizeof(char), n, cfddns.fout);
     }
     fclose(cfddns.fin);
     fclose(cfddns.fout);
-
     return 0;
 }
